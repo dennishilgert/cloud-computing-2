0b350a015673f6df1aa8acb29019b0bfd005a3e0
dennis <dennis@hilgerts.de>
2024-03-18 17:20:14 +0000

    Adding prerequisites check to deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,15 +1,36 @@
 #!/bin/sh
 
+echo "Starting Deployment process"
+
+# Check if both files exist
+echo "Checking prerequisites ..."
+if [ ! -f "../service-account.json" ] || [ ! -f "./main.tf" ]; then
+    echo "Please make sure the following files exist:"
+    echo "deployment/main.tf"
+    echo "service-account.json"
+    echo "Both can be created from the respective samples."
+fi
+
+# Remove sample terraform file
+echo "Removing sample Terraform file ..."
+rm main.sample.tf
+
 # Apply Terraform Configuration
+echo "Initializing Terraform and applying configuration ..."
 terraform init && terraform apply -auto-approve
 
 # Get VM IP and Redis IP
+echo "Reading output variables of Terraform execution ..."
 VM_IP=$(terraform output -raw vm_ip)
 REDIS_IP=$(terraform output -raw redis_ip)
 
 # Create Ansible Inventory
+echo "Creating dynamic Ansible inventory ..."
 echo "[gcp_vm]" > ansible_inventory
 echo "$VM_IP ansible_user=dennis ansible_ssh_private_key_file=~/.ssh/id_rsa" >> ansible_inventory
 
 # Run Ansible Playbook
+echo "Running Ansible playbook ..."
 ansible-playbook -i ansible_inventory playbook.yml -e redis_host=$REDIS_IP
+
+echo "Deployed."
